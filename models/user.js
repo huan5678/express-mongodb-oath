b1c84d31c83d7f536fbd9965eb4cb3dc2c15250b
黃士桓 <huan5678@gmail.com>
2022-05-17 01:03:04 +0800

    [update]

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = {
   email: {
@@ -12,18 +13,18 @@ const userSchema = {
   },
   password: {
     type: String,
-    minLength: 8,
+    minLength: [8, '密碼至少 8 個字'],
     required: [true, '密碼欄位，請確實填寫'],
     matches: [
       /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,}$/,
-      '密碼需符合至少有 1 個數字， 1 個大寫英文， 1 個小寫英文及 1 個特殊符號規定，至少 8 碼',
+      '密碼需符合至少有 1 個數字， 1 個大寫英文， 1 個小寫英文及 1 個特殊符號規定',
     ],
   },
   name: {
     type: String,
     required: [true, '名稱為必要資訊'],
-    minLength: 3,
-    maxLength: 50,
+    minLength: [1, '名稱請大於 1 個字'],
+    maxLength: [50, '名稱長度過長，最多只能 50 個字'],
   },
   photo: String,
   gender: {
@@ -35,16 +36,23 @@ const userSchema = {
     default: Date.now,
     select: false,
   },
-  thirdPartyAuthor: {
-    googleId: String,
-    facebookId: String,
-    lineId: String,
-    githubId: String,
+  googleId: String,
+  facebookId: String,
+  lineId: String,
+  githubId: String,
+  isValidator: {
+    type: Boolean,
+    default: false,
   },
 };
 
 const User_Schema = new mongoose.Schema(userSchema, {
   versionKey: false,
+});
+
+User_Schema.pre('save', function () {
+  const salt = bcrypt.genSaltSync(8);
+  this.password = bcrypt.hashSync(this.password, salt);
 });
 
 const User = mongoose.model('User', User_Schema);
--- routes/auth.js
+++ routes/auth.js
@@ -2,21 +2,23 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 const axios = require('axios');
-const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const apiSecret = process.env.AUTH_SECRET;
+const jwtSecret = process.env.JWT_SECRET;
+const jwtExpires = process.env.JWT_EXPIRES_DAY;
 
-const handleErrorAsync = require('../utils/handleErrorAsync');
+const asyncWrapper = require('../middleware/async');
 const successHandle = require('../utils/successHandle');
 const appError = require('../utils/appError');
+const {isAuthor, generateToken} = require('../middleware/handleAuthor');
 
 const passport = require('passport');
 const {v4: uuidv4} = require('uuid');
 
 router.post(
   '/account/create',
-  handleErrorAsync(async (req, res, next) => {
+  asyncWrapper(async (req, res, next) => {
     let {email, password, confirmPassword, name} = req.body;
     if (!email || !password || !confirmPassword || !name) {
       return appError(400, '欄位未正確填寫', next);
@@ -35,15 +37,13 @@ router.post(
     if (!passwordRules.test(password)) {
       return appError(
         400,
-        '密碼強度不足，請確認是否具至少有 1 個數字， 1 個大寫英文， 1 個小寫英文及 1 個特殊符號，密碼長度需超過 8 個字',
+        '密碼強度不足，請確認是否具至少有 1 個數字， 1 個大寫英文， 1 個小寫英文及 1 個特殊符號，密碼長度需 8 個字以上',
         next
       );
     }
     if (password !== confirmPassword) {
       return appError(400, '請確認兩次輸入的密碼是否相同', next);
     }
-    const salt = bcrypt.genSaltSync(8);
-    password = bcrypt.hashSync(req.body.password, salt);
     const userData = {
       name,
       email,
@@ -54,40 +54,28 @@ router.post(
   })
 );
 
-const isAuthor = handleErrorAsync(async (req, res, next) => {
-  const accessToken = req.header('Authorization').split('Bearer ').pop();
-  if (!accessToken) {
-    return appError(401, '未帶入驗證碼，請重新登入！', next);
-  }
-  const decoded = jwt.verify(accessToken, apiSecret);
-  const checkDate = (time) => {
-    const now = new Date().getTime();
-    if (now > time * 1000) {
-      return appError(400, '驗證碼已過期，請重新登入！', next);
-    }
-  };
-  checkDate(decoded.exp);
-  req.user = decoded;
-  next();
-});
-
 router.post(
   '/account/login',
-  handleErrorAsync(async (req, res, next) => {
+  asyncWrapper(async (req, res, next) => {
     const {email, password} = req.body;
     if (!email || !password) {
-      return appError(400, '欄位未正確填寫', next);
+      return appError(400, 'email 或 password 欄位未正確填寫', next);
     }
     const user = await User.findOne({email});
     if (!user) {
       return appError(404, '無此使用者資訊請確認 email 帳號是否正確', next);
+    }
+    const userPassword = await User.findOne({email}).select('+password');
+    const checkPassword = bcrypt.compare(req.body.password, userPassword);
+    if (!checkPassword) {
+      return appError(400, '請確認密碼是否正確，請再嘗試輸入', next);
     }
     const payload = {
       id: user._id,
       name: user.name,
       photo: user.photo,
     };
-    const token = jwt.sign(payload, apiSecret, {expiresIn: '7 day'});
+    const token = jwt.sign(payload, jwtSecret, {expiresIn: jwtExpires});
     return successHandle(res, '登入成功', token);
   })
 );
@@ -95,7 +83,7 @@ router.post(
 router.get(
   '/account/test',
   isAuthor,
-  handleErrorAsync(async (req, res, next) => {
+  asyncWrapper(async (req, res, next) => {
     console.log(req.user);
     successHandle(res, '驗證登入', req.user);
   })
@@ -275,7 +263,7 @@ router.get('/account/github', (req, res) => {
 
 router.get(
   '/account/github/callback',
-  handleErrorAsync(async (req, res, next) => {
+  asyncWrapper(async (req, res, next) => {
     const code = req.query.code;
     const options = {
       code,
